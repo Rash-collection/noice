5aef7dafb028f6111d3cf170b735fb84a187ca1d
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-04 19:16:35 +0300

    moved functional interfaces..

--- src/main/java/noice/interact/scenes/functionals/Resizable.java
+++ src/main/java/noice/interact/scenes/functionals/Resizable.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package noice.interact;
+package noice.interact.scenes.functionals;
 
 /**
  *
  * @author rash4
  */
 @FunctionalInterface
-public interface Scalable {
-    void scales(float scaleFactor);
+public interface Resizable {
+    void resized(java.awt.Dimension containerSize);
 }
--- src/main/java/noice/interact/scenes/functionals/Updatable.java
+++ src/main/java/noice/interact/scenes/functionals/Updatable.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package noice.interact;
+package noice.interact.scenes.functionals;
 
 /**
  *
  * @author rash4
  */
 @FunctionalInterface
-public interface Paintable {
-    void painting(java.awt.Graphics2D grr);
+public interface Updatable {
+    void updating(int incremental);
 }
