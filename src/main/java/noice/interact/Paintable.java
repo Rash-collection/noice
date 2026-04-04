22c4121e82001223242a8952e2fae37661b5913f
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-04 03:36:40 +0300

    painting functional-interface

---
+++ src/main/java/noice/interact/Paintable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package noice.interact;
+
+/**
+ *
+ * @author rash4
+ */
+@FunctionalInterface
+public interface Paintable {
+    void painting(java.awt.Graphics2D grr);
+}
