ec97999b2d8a99eb23d845f4552ee8b21a3febbb
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-04 03:38:37 +0300

    scaling functional-interface

---
+++ src/main/java/noice/interact/Scalable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package noice.interact;
+
+/**
+ *
+ * @author rash4
+ */
+@FunctionalInterface
+public interface Scalable {
+    void scales(float scaleFactor);
+}
