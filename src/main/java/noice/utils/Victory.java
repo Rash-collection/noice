e8534987a958315ddfaea23ca8eb8c341fce500b
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-07 02:16:16 +0300

    added lesser/greater methods, two for booleans and two for returning the object with the conditions met.

--- src/main/java/noice/utils/Victory.java
+++ src/main/java/noice/utils/Victory.java
@@ -20,6 +20,12 @@ public record Victory(float x, float y) implements Comparable<Victory>{
     public Victory(Victory neo){
         this(neo.x, neo.y);
     }
+    
+    public boolean isGreater(Victory other) { return this.compareTo(other) > 0; }
+    public boolean isLlesser(Victory other) { return this.compareTo(other) < 0; }
+    public Victory greater(Victory other) { return this.compareTo(other) > 0 ? this : other; }
+    public Victory lesser(Victory other) { return this.compareTo(other) < 0 ? this : other; }
+    
     public Victory add(float x, float y){
         return new Victory(this.x + x, this.y + y);
     }
@@ -73,7 +79,6 @@ public record Victory(float x, float y) implements Comparable<Victory>{
         return new Victory(this.x / length, this.y / length);
     }
     public Point toPoint(){return new Point((int)this.x, (int)this.y);} 
-    
     @Override public String toString(){
         return String.format(("%s(x[%.4f]:y[%.4f])"), this.getClass().getSimpleName(), this.x, this.y);
     }
