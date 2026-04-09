0adba02aa7ea7046fc8c0ce21d24ffb3af9fb6de
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-09 23:28:55 +0300

    a concrete class for Inpux

---
+++ src/main/java/noice/interact/InputsAdapt.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package noice.interact;
+
+/**
+ * concrete class of {@link Inpux}
+ * @author rash4
+ */
+public class InputsAdapt<T extends Inpux<T>> extends Inpux<T>{
+}
