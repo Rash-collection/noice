88671ec8e6072bedd83fb56420d8f2b1daedfdf5
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-10 08:06:25 +0300

    .

---
+++ src/main/java/noice/entities/Animatable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package noice.entities;
+
+/**
+ *
+ * @author rash4
+ */
+@FunctionalInterface
+public interface Animatable {
+    // not ready yet, might add a param for a globalized index @@!?
+    java.awt.image.BufferedImage currentImage();
+}
