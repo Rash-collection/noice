8cd14659c30c75f75f4f16ef46350966882527ec
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-11 03:38:10 +0300

    now we have the first scene to say the least -> noice.debug.Display

--- src/main/java/noice/Noice.java
+++ src/main/java/noice/Noice.java
@@ -4,13 +4,18 @@
 
 package noice;
 
+import debug.Display;
+import noice.interact.Channel;
+
 /**
  *
  * @author rash4
  */
 public class Noice {
     public static void main(String[] args) {
-        noice.interact.Channel.initalize();
+        final Display dis = Display.display(0, 0, 800, 600);
+        dis.initialize();
+        Channel.initalize();
         System.out.println("Hello World!");
         
     }
